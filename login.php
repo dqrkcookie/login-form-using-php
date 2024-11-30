ffb597ff113d86200f3f61f793320448cfd432f8
DarkCookie <crislester27@gmail.com>
2024-12-01 07:30:34 +0800

    second commit

--- login.php
+++ login.php
@@ -43,7 +43,7 @@ if (isset($_POST["submit"])) {
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Dodoco</title>
+    <title>Dodoco Tails</title>
 </head>
 <style>
     * {
